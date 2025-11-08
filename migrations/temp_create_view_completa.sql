9be51f7b1d6bfee612cf652f1f23f5ac16071c7f
vpcapanema <vpcapanema@outlook.com>
2025-11-08 14:10:33 -0300

    refactor: remover IDs duplicados da view v_pesquisas_completa
    VIEW OTIMIZADA (80 colunas):
    - Mantem apenas id_pesquisa como ID unico visivel
    - Remove IDs duplicados: emp_id_empresa, ent_id_entrevistado, entv_id_entrevistador, inst_id_instituicao
    - Mantem id_empresa, id_entrevistado, id_responsavel da tabela pesquisas (necessarios para relacionamento)
    - Mantem emp_id_municipio (dado relevante para analise)
    
    Reducao: 122 colunas -> 80 colunas (mais limpo e eficiente)
    IDs servem apenas para JOINs internos, nao precisam aparecer duplicados na view.

--- migrations/temp_create_view_completa.sql
+++ migrations/temp_create_view_completa.sql
@@ -3,10 +3,58 @@ DROP VIEW IF EXISTS formulario_embarcadores.v_pesquisas_completa CASCADE;
 CREATE VIEW formulario_embarcadores.v_pesquisas_completa AS
 SELECT 
     -- TABELA PESQUISAS (TODAS AS COLUNAS)
-    p.*,
+    p.id_pesquisa,
+    p.id_empresa,
+    p.id_entrevistado,
+    p.tipo_responsavel,
+    p.id_responsavel,
+    p.data_entrevista,
+    p.data_atualizacao,
+    p.status,
+    p.produto_principal,
+    p.agrupamento_produto,
+    p.outro_produto,
+    p.tipo_transporte,
+    p.origem_pais,
+    p.origem_estado,
+    p.origem_municipio,
+    p.destino_pais,
+    p.destino_estado,
+    p.destino_municipio,
+    p.distancia,
+    p.tem_paradas,
+    p.modos,
+    p.config_veiculo,
+    p.peso_carga,
+    p.unidade_peso,
+    p.custo_transporte,
+    p.valor_carga,
+    p.tipo_embalagem,
+    p.carga_perigosa,
+    p.tempo_dias,
+    p.tempo_horas,
+    p.tempo_minutos,
+    p.frequencia,
+    p.frequencia_outra,
+    p.importancia_custo,
+    p.variacao_custo,
+    p.importancia_tempo,
+    p.variacao_tempo,
+    p.importancia_confiabilidade,
+    p.variacao_confiabilidade,
+    p.importancia_seguranca,
+    p.variacao_seguranca,
+    p.importancia_capacidade,
+    p.variacao_capacidade,
+    p.tipo_cadeia,
+    p.modais_alternativos,
+    p.fator_adicional,
+    p.dificuldades,
+    p.detalhe_dificuldade,
+    p.observacoes,
+    p.num_paradas,
     
-    -- TABELA EMPRESAS (TODAS AS COLUNAS COM PREFIXO emp_)
-    e.id_empresa AS emp_id_empresa,
+    -- TABELA EMPRESAS (SEM id_empresa, COM PREFIXO emp_)
     e.tipo_empresa AS emp_tipo_empresa,
     e.outro_tipo AS emp_outro_tipo,
     e.municipio AS emp_municipio,
@@ -25,9 +73,7 @@ SELECT
     e.cep AS emp_cep,
     e.cnpj AS emp_cnpj,
     
-    -- TABELA ENTREVISTADOS (TODAS AS COLUNAS COM PREFIXO ent_)
-    ent.id_entrevistado AS ent_id_entrevistado,
-    ent.id_empresa AS ent_id_empresa,
+    -- TABELA ENTREVISTADOS (SEM id_entrevistado e id_empresa, COM PREFIXO ent_)
     ent.nome AS ent_nome,
     ent.funcao AS ent_funcao,
     ent.telefone AS ent_telefone,
@@ -37,14 +83,11 @@ SELECT
     ent.data_atualizacao AS ent_data_atualizacao,
     ent.email_lower AS ent_email_lower,
     
-    -- TABELA ENTREVISTADORES (TODAS AS COLUNAS COM PREFIXO entv_ - apenas se tipo_responsavel = 'entrevistador')
-    entv.id_entrevistador AS entv_id_entrevistador,
+    -- TABELA ENTREVISTADORES (SEM id_entrevistador e id_instituicao, COM PREFIXO entv_)
     entv.nome_completo AS entv_nome_completo,
     entv.email AS entv_email,
-    entv.id_instituicao AS entv_id_instituicao,
     
-    -- TABELA INSTITUICOES (TODAS AS COLUNAS COM PREFIXO inst_ - apenas se tipo_responsavel = 'entrevistador')
-    inst.id_instituicao AS inst_id_instituicao,
+    -- TABELA INSTITUICOES (SEM id_instituicao, COM PREFIXO inst_)
     inst.nome_instituicao AS inst_nome_instituicao,
     inst.tipo_instituicao AS inst_tipo_instituicao,
     inst.cnpj AS inst_cnpj
